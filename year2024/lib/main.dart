a0345a4abd34a9c5c16223a2385c65f03daad07a
Samu <csonka.samuel@gmail.com>
2023-12-28 21:20:03 +0100

    code cleanup

--- year2024/lib/main.dart
+++ year2024/lib/main.dart
@@ -30,44 +30,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _formKey,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("App BAR!"),
+        leading: const Icon(Icons.menu),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: 'Your New Year Wish',
-              ),
-              validator: (String? value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // Process data.
-                  }
-                },
-                child: const Text('Add'),
-              ),
-            ),
-          ],
+      body: Column(
+        children: [
+          TextForm(),
+        ],
+      ),
+    );
+  }
+}
+
+class TextForm extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  TextForm({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      key: _formKey,
+      children: [
+        TextFormField(
+          decoration: const InputDecoration(
+            hintText: 'Your New Year Wish',
+          ),
+          validator: (String? value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter some text';
+            }
+            return null;
+          },
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                // Process data.
+              }
+            },
+            child: const Text('Add'),
+          ),
+        ),
+      ],
     );
   }
 }
